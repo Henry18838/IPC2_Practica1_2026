996b38e00fce0bd91f9cca59e56705852a76896c
Henry18838 <champet258@gmail.com>
2026-03-06 19:51:46 -0600

    Estructura inicial del proyecto con Maven

---
+++ pizza-express-tycoon/src/main/java/com/ipc2/App.java
@@ -0,0 +1,13 @@
+package com.ipc2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
