f5b8784a3d97813764541b09e02a7e746d52526e
Henry18838 <champet258@gmail.com>
2026-03-10 21:10:56 -0600

    Modificacion Visual

--- pizza-express-tycoon/src/main/java/com/ipc2/App.java
+++ pizza-express-tycoon/src/main/java/com/ipc2/App.java
@@ -1,13 +1,21 @@
 package com.ipc2;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import com.ipc2.view.LoginFrame;
+
+import javax.swing.UIManager;
 
 public class App {
 
     public static void main(String[] args) {
 
-        LoginFrame loginFrame = new LoginFrame();
-        loginFrame.setVisible(true);
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception e) {
+            System.out.println("No se pudo aplicar el tema oscuro");
+            e.printStackTrace();
+        }
 
+        new LoginFrame().setVisible(true);
     }
 }
